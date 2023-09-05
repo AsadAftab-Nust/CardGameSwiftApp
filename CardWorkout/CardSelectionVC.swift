efe30cc6ad26886f4dbd8d1ec05ff7ff18fdde72
Asad Aftab <aaftab.msee18seecs@seecs.edu.pk>
2023-09-05 19:00:55 +0200

    Card Game
    This is my First ever App using Swift storyboard

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -8,14 +8,59 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-
+    
+    @IBOutlet var cardImageView: UIImageView!
+    
+    
+    // Single Outlet
+    // @IBOutlet var stopButton: UIButton!
+    // @IBOutlet var restartButton: UIButton!
+    // @IBOutlet var rulesButton: UIButton!
+    
+    
+    var timer: Timer!
+    var cards: [UIImage] = Card.allValues
+    @IBOutlet var buttons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // When you want separate properties to be added
+        //stopButton.layer.cornerRadius = 8
+        //restartButton.layer.cornerRadius = 8
+        //rulesButton.layer.cornerRadius = 8
+        startTimer()
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
     }
     
-
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        // stopButton.setTitle("I've been tapped", for: .normal)
+        
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+    
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
     /*
     // MARK: - Navigation
 
